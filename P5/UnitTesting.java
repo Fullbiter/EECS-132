8a3fdbe0e7a2ebe06cdb5deff11dfc030a9f872e
Kevin Nash <kjn33@case.edu>
2015-04-25 18:46:49 -0400

    Card and Deck complete, functional, and tested

--- P5/UnitTesting.java
+++ P5/UnitTesting.java
@@ -18,95 +18,17 @@ public class UnitTesting {
     @Test
     public void cardConstructor() {
         
-        // Number of unique face values
-        final int FACES_COUNT = 13;
-        // Number of unique suits
-        final int SUITS_COUNT = 4;
-        
         Card.Face[] allFaces = Card.Face.values();
         Card.Suit[] allSuits = Card.Suit.values();
-        // Expected array to be compared against
-        String[] expectedStrings = new String[FACES_COUNT * SUITS_COUNT];
-        // Experimental array to compare
-        String[] testStrings = new String[FACES_COUNT * SUITS_COUNT];
-        // Placeholder for the expected Face
-        String expectedFace = "";
-        // Placeholder for the expected Suit
-        String expectedSuit = "";
-        // Placeholder for the experimental Face
-        String testFace = "";
-        // Placeholder for the experimental Suit
-        String testSuit = "";
-        // The current index
+        String[] testStrings = new String[allFaces.length * allSuits.length];
+        String[] expectedStrings = {"AS", "AH", "AD", "AC", "2S", "2H", "2D", "2C", "3S", "3H", "3D", "3C", "4S", "4H",
+                                    "4D", "4C", "5S", "5H", "5D", "5C", "6S", "6H", "6D", "6C", "7S", "7H", "7D", "7C",
+                                    "8S", "8H", "8D", "8C", "9S", "9H", "9D", "9C", "10S", "10H", "10D", "10C", "JS",
+                                    "JH", "JD", "JC", "QS", "QH", "QD", "QC", "KS", "KH", "KD", "KC"};
         int index = 0;
-        
-        // For each face ...
-        for (int i = 1; i <= FACES_COUNT; i++) {
-            switch (i) {
-                case 1:  testFace = "ace";
-                         expectedFace = "A";
-                         break;
-                case 2:  testFace = "two";
-                         expectedFace = "2";
-                         break;
-                case 3:  testFace = "three";
-                         expectedFace = "3";
-                         break;
-                case 4:  testFace = "four";
-                         expectedFace = "4";
-                         break;
-                case 5:  testFace = "five";
-                         expectedFace = "5";
-                         break;
-                case 6:  testFace = "six";
-                         expectedFace = "6";
-                         break;
-                case 7:  testFace = "seven";
-                         expectedFace = "7";
-                         break;
-                case 8:  testFace = "eight";
-                         expectedFace = "8";
-                         break;
-                case 9:  testFace = "nine";
-                         expectedFace = "9";
-                         break;
-                case 10: testFace = "ten";
-                         expectedFace = "10";
-                         break;
-                case 11: testFace = "jack";
-                         expectedFace = "J";
-                         break;
-                case 12: testFace = "queen";
-                         expectedFace = "Q";
-                         break;
-                case 13: testFace = "king";
-                         expectedFace = "K";
-                         break;
-            }
-            // ... and each suit ...
-            for (int j = 1; j <= SUITS_COUNT; j++) {
-                switch (j) {
-                    case 1: testSuit = "spades";
-//                            expectedSuit = "\u2660";
-                            expectedSuit = "S";
-                            break;
-                    case 2: testSuit = "hearts";
-//                            expectedSuit = "\u2665";
-                            expectedSuit = "H";
-                            break;
-                    case 3: testSuit = "diamonds";
-//                            expectedSuit = "\u2666";
-                            expectedSuit = "D";
-                            break;
-                    case 4: testSuit = "clubs";
-//                            expectedSuit = "\u2663";
-                            expectedSuit = "C";
-                            break;
-                }
-                // ... create a card with the current combination
-                testStrings[index] = new Card(testFace, testSuit).toString();
-                expectedStrings[index++] = expectedFace + expectedSuit;
-            }
+        for (Card.Face face : allFaces) {
+            for (Card.Suit suit : allSuits)
+                testStrings[index++] = new Card(face, suit).toString();
         }
         assertArrayEquals("One or more cards are displayed improperly", expectedStrings, testStrings);
     }
@@ -157,25 +79,26 @@ public class UnitTesting {
         deck = new Deck(Card.Face.JACK, Card.Face.KING, Card.Suit.DIAMONDS, Card.Suit.CLUBS);
         assertEquals("A Deck of last edge Faces and Suits is not displayed properly",
                      "JD  JC\nQD  QC\nKD  KC", deck.toString());
+        // test one Face and one Suit
+        deck = new Deck(Card.Face.ACE, Card.Face.ACE, Card.Suit.SPADES, Card.Suit.SPADES);
+        assertEquals("A Deck of last edge Faces and Suits is not displayed properly",
+                     "AS\nAS", deck.toString());
     }
     
     /**
      * Tests the getter methods of Deck
      */
-//    @Test
-//    public void deckGetter() {
-//        Deck deck = new Deck("ace", "spades");
-//        // "ace" = "ace"
-//        assertTrue("Error in method getFace", card1.getFace().equals(card2.getFace()));
-//        // "ace" = "two"
-//        assertFalse("Error in method getFace", card1.getFace().equals(card3.getFace()));
-//        // "spades" = "spades"
-//        assertTrue("Error in method getSuit", card1.getSuit().equals(card2.getSuit()));
-//        // "spades" = "hearts"
-//        assertFalse("Error in method getSuit", card1.getSuit().equals(card3.getSuit()));
-//        card1.setIsFaceUp(true);
-//        assertTrue("Getter incorrectly states that the card is face down", card1.getIsFaceUp());
-//        card1.setIsFaceUp(false);
-//        assertFalse("Getter incorrectly states that the card is face up", card1.getIsFaceUp());
-//    }
+    @Test
+    public void deckGetter() {
+        // default
+        Deck deck = new Deck();
+        assertEquals("Method getNumberSuits does not count correctly", 4, deck.getNumberSuits());
+        assertEquals("Method getMinFace returns an incorrect Face", Card.Face.ACE, deck.getMinFace());
+        assertEquals("Method getMaxFace returns an incorrect Face", Card.Face.KING, deck.getMaxFace());
+        // duplicate Suits and one Face
+        deck = new Deck(Card.Face.ACE, Card.Face.ACE, Card.Suit.SPADES, Card.Suit.SPADES);
+        assertEquals("Method getNumberSuits counts duplicate Suits", 1, deck.getNumberSuits());
+        assertEquals("Method getMinFace returns an incorrect Face", Card.Face.ACE, deck.getMinFace());
+        assertEquals("Method getMaxFace returns an incorrect Face", Card.Face.ACE, deck.getMaxFace());
+    }
 }
