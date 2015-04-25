f39751ce987ed3bc4bbfc092179891c20beff901
Kevin Nash <kjn33@case.edu>
2015-04-24 23:48:18 -0400

    Created Card class and testing for it

---
+++ P5/Card.java
@@ -0,0 +1,183 @@
+/**
+ * Card objects represent a playing card.
+ *
+ * @author   Kevin Nash (kjn33)
+ * @version  2015.4.24
+ */
+public class Card {
+    
+    /** Whether the card is face up or face down **/
+    private boolean isFaceUp = true;
+    
+    /** The face value of the card **/
+    private Face face;
+    
+    /** The suit of the card **/
+    private Suit suit;
+    
+    /** enum for the possible face values of the card **/
+    private enum Face {
+        ACE("Ace"), TWO("Two"), THREE("Three"), FOUR("Four"), FIVE("Five"), SIX("Six"), SEVEN("Seven"),
+        EIGHT("Eight"), NINE("Nine"), TEN("Ten"), JACK("Jack"), QUEEN("Queen"), KING("King");
+        
+        /** the name of the Face **/
+        private String name;
+        /** the symbol of the Face **/
+        private String symbol;
+        
+        private Face(String name) {
+            this.name = name;
+            switch (name.toLowerCase()) {
+                case "ace":   this.symbol = "A";
+                    break;
+                case "two":   this.symbol = "2";
+                    break;
+                case "three": this.symbol = "3";
+                    break;
+                case "four":  this.symbol = "4";
+                    break;
+                case "five":  this.symbol = "5";
+                    break;
+                case "six":   this.symbol = "6";
+                    break;
+                case "seven": this.symbol = "7";
+                    break;
+                case "eight": this.symbol = "8";
+                    break;
+                case "nine":  this.symbol = "9";
+                    break;
+                case "ten":   this.symbol = "10";
+                    break;
+                case "jack":  this.symbol = "J";
+                    break;
+                case "queen": this.symbol = "Q";
+                    break;
+                case "king":  this.symbol = "K";
+                    break;
+            }
+        }
+        
+        /**
+         * Returns the Face bearing the given name
+         * @param  name  the name of the Face
+         * @return  Face of name
+         */        
+        public static Face getFaceByName(String name) throws IllegalArgumentException {
+            try {
+                return Face.valueOf(name.toUpperCase());
+            }
+            catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("The face value \"" + name + "\" was not found", e);
+            }
+        }
+        
+        /**
+         * Returns the symbolic representation of the Face
+         * @return Face symbol
+         */
+        @Override
+        public String toString() {
+            return this.symbol;
+        }
+    }
+    
+    /** enum for the possible suits of the card **/
+    private enum Suit {
+        SPADES("Spades"), HEARTS("Hearts"), DIAMONDS("Diamonds"), CLUBS("Clubs");
+        
+        /** the name of the Suit **/
+        private String name;
+        /** the symbol of the Suit **/
+        private String symbol;
+        /** the color of the Suit **/
+        private String color;
+        
+        /**
+         * Constructs a Suit
+         * @param  name  Suit name
+         */
+        private Suit(String name) {
+            this.name = name;
+            switch (name.toLowerCase()) {
+                case "spades":
+                    this.symbol = "\u2660";
+                    this.color = "black";
+                    break;
+                case "hearts":
+                    this.symbol = "\u2665";
+                    this.color = "red";
+                    break;
+                case "diamonds":
+                    this.symbol = "\u2666";
+                    this.color = "red";
+                    break;
+                case "clubs":
+                    this.symbol = "\u2663";
+                    this.color = "black";
+                    break;
+            }
+        }
+        
+        /**
+         * Returns the color of the Suit
+         * @return  Suit color
+         */
+        public String getColor() {
+            return this.color;
+        }
+        
+        /**
+         * Returns the Suit bearing the given name
+         * @param  name  the name of the Suit
+         * @return  Suit of name
+         * @throws  IllegalArgumentException  given an illegal name
+         */
+        public static Suit getSuitByName(String name) throws IllegalArgumentException {
+            try {
+                return Suit.valueOf(name.toUpperCase());
+            }
+            catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("The suit value \"" + name + "\" was not found", e);
+            }
+        }
+        
+        /**
+         * Returns the symbolic representation of the Suit
+         * @return Suit symbol
+         */
+        @Override
+        public String toString() {
+            return this.symbol;
+        }
+    }
+    
+    /**
+     * Constructs a Card
+     * @param  face  the face value of the card
+     * @param  suit  the suit of the card
+     */
+    public Card(String face, String suit) {
+        this(face, suit, true);
+    }
+    
+    /**
+     * Constructs a Card
+     * @param  face      the face value of the card
+     * @param  suit      the suit of the card
+     * @param  isFaceUp  whether the card is face up
+     */
+    public Card(String face, String suit, Boolean isFaceUp) {
+        this.face = Face.getFaceByName(face);
+        this.suit = Suit.getSuitByName(suit);
+        this.isFaceUp = isFaceUp;
+    }
+    
+    /**
+     * Returns the symbolic representation of the Card
+     * @return Card symbol
+     */
+    @Override
+    public String toString() {
+        return this.face.toString() + this.suit.toString();
+    }
+}
---
+++ P5/UnitTesting.java
@@ -0,0 +1,94 @@
+/**
+ * The UnitTesting class provides JUnit testing for Project 5.
+ *
+ * @author   Kevin Nash (kjn33)
+ * @version  2015.4.24
+ */
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UnitTesting {
+    
+    /**
+     * Tests the creation of every kind of Card (default number is 52)
+     */
+    @Test
+    public void cardConstructor() {
+        final int FACES_COUNT = 13;
+        final int SUITS_COUNT = 4;
+        String[] expectedStrings = new String[FACES_COUNT * SUITS_COUNT];
+        String[] testStrings = new String[FACES_COUNT * SUITS_COUNT];
+        String expectedFace = "";
+        String expectedSuit = "";
+        String testFace = "";
+        String testSuit = "";
+        int index = 0;
+        // For each face ...
+        for (int i = 1; i <= FACES_COUNT; i++) {
+            switch (i) {
+                case 1:  testFace = "ace";
+                         expectedFace = "A";
+                         break;
+                case 2:  testFace = "two";
+                         expectedFace = "2";
+                         break;
+                case 3:  testFace = "three";
+                         expectedFace = "3";
+                         break;
+                case 4:  testFace = "four";
+                         expectedFace = "4";
+                         break;
+                case 5:  testFace = "five";
+                         expectedFace = "5";
+                         break;
+                case 6:  testFace = "six";
+                         expectedFace = "6";
+                         break;
+                case 7:  testFace = "seven";
+                         expectedFace = "7";
+                         break;
+                case 8:  testFace = "eight";
+                         expectedFace = "8";
+                         break;
+                case 9:  testFace = "nine";
+                         expectedFace = "9";
+                         break;
+                case 10: testFace = "ten";
+                         expectedFace = "10";
+                         break;
+                case 11: testFace = "jack";
+                         expectedFace = "J";
+                         break;
+                case 12: testFace = "queen";
+                         expectedFace = "Q";
+                         break;
+                case 13: testFace = "king";
+                         expectedFace = "K";
+                         break;
+            }
+            // ... and each suit ...
+            for (int j = 1; j <= SUITS_COUNT; j++) {
+                switch (j) {
+                    case 1: testSuit = "spades";
+                            expectedSuit = "\u2660";
+                            break;
+                    case 2: testSuit = "hearts";
+                            expectedSuit = "\u2665";
+                            break;
+                    case 3: testSuit = "diamonds";
+                            expectedSuit = "\u2666";
+                            break;
+                    case 4: testSuit = "clubs";
+                            expectedSuit = "\u2663";
+                            break;
+                }
+                // ... create a card with the current combination
+                testStrings[index] = new Card(testFace, testSuit).toString();
+                expectedStrings[index++] = expectedFace + expectedSuit;
+            }
+        }
+        assertArrayEquals("One or more cards is displayed improperly",
+                          expectedStrings, testStrings);
+    }
+}
